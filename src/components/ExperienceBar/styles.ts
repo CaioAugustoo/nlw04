699162f57f21cd9a78d9b22b2d33d326b61dd205
caioaugustoo <caioamfr@gmail.com>
2021-02-23 14:19:32 -0300

    Criado componente de XP

--- src/components/ExperienceBar/styles.ts
+++ src/components/ExperienceBar/styles.ts
@@ -1,9 +1,39 @@
 import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.header``
+export const Wrapper = styled.header`
+  ${({theme}) => css`
+    display: flex;
+    align-items: center;
 
-export const Experience = styled.span``
+    > div {
+      flex: 1;
+      height: 4px;
+      border-radius: ${theme.border.radius01};
+      background: ${theme.colors.gray};
+      margin: 0 1.5rem;
+      position: relative;
+    }
+  `}
+`
 
-export const CurrentExperienceWrapper = styled.div``
+export const Experience = styled.span`
+  ${({theme}) => css`
+    font-size: ${theme.font.sizes.normal};
+  `}
+`
 
-export const CurrentExperience = styled.span``
+export const CurrentExperienceWrapper = styled.div`
+  ${({theme}) => css`
+    > div {
+      height: 4px;
+      border-radius: ${theme.border.radius01};
+      background: ${theme.colors.green};
+    }
+  `}
+`
+
+export const CurrentExperience = styled.span`
+  position: absolute;
+  top: 12px;
+  transform: translateX(-50%);
+`
