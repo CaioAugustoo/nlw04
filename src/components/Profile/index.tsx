bc1698079bfc310baae9ea67b0ab72b5f992eb4a
caioaugustoo <caioamfr@gmail.com>
2021-02-23 14:20:06 -0300

    Criado componente de perfil

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -2,7 +2,18 @@ import React from "react";
 import * as S from "./styles";
 
 const Profile = () => {
-  return <div></div>;
+  return (
+    <S.Wrapper>
+      <img src="https://github.com/caioaugustoo.png" alt="Pedro Giampietro" />
+      <div>
+        <strong>Caio Augusto</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </div>
+    </S.Wrapper>
+  );
 };
 
 export default Profile;
--- src/components/Profile/styles.ts
+++ src/components/Profile/styles.ts
@@ -1,1 +1,28 @@
 import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.div`
+  ${({theme}) => css`
+    display: flex;
+    align-items: center;
+
+    > img {
+      width: 5.5rem;
+      height: 5.5rem;
+      border-radius: ${theme.border.radius02};
+    }
+
+    div {
+      margin-left: 1.5rem;
+    }
+
+    div strong {
+      font-size: ${theme.font.sizes.xlarge};
+      font-weight: ${theme.font.bold};
+      color: ${theme.colors.title};
+    }
+
+    div p img {
+      margin-right: 0.5rem;
+    }
+  `}
+`
