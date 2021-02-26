771cfb2209ae1e65ab7db89ebe9f3cb83b6e15cd
caioaugustoo <caioamfr@gmail.com>
2021-02-26 17:42:41 -0300

    Alterado valor inicial do countdown

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -28,7 +28,7 @@ export const CountDownContext = createContext({} as CountdownContextData);
 export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
